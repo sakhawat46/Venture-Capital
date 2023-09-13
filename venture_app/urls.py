8b13a01106d5159edad8b83f89bf5934c99c914f
sakhawat46 <hsakhawat135@gmail.com>
2023-09-13 16:47:41 +0600

    first commit

---
+++ venture_app/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from venture_app import views
+
+app_name = "venture_app"
+
+urlpatterns = [
+    # path('register/', views.register, name='register'),
+    # path('login/', views.customerlogin, name='login'),
+    # path('logout/', views.logout_user, name='logout'),
+    # path('profile/', views.profile, name='profile'),
+
+]
