5b3a4aefb37f9a1e3c57db21acfe04d76a546570
sakhawat46 <hsakhawat135@gmail.com>
2023-09-14 12:34:01 +0600

    home page

--- venture_app/urls.py
+++ venture_app/urls.py
@@ -4,7 +4,7 @@ from venture_app import views
 app_name = "venture_app"
 
 urlpatterns = [
-    # path('register/', views.register, name='register'),
+    path('', views.home, name='home'),
     # path('login/', views.customerlogin, name='login'),
     # path('logout/', views.logout_user, name='logout'),
     # path('profile/', views.profile, name='profile'),
